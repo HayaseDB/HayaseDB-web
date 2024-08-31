b106a8331c46c39929d6d780cf159c6508013072
Sebastian-Stepper <Sebastian.Stepper@mr-pixel.de>
2024-08-31 21:25:27 +0200

    added login view

--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './views/HomeView.vue';
+import Login from "@/views/LoginView.vue";
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
     },
 ];
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    test
+  </div>
+</template>
+
+<style scoped>
+
+.container{
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
