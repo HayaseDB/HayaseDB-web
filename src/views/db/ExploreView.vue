2cdafd646edbd660c00f13256a4c6721dad7521d
Sebastian-Stepper <Sebastian.Stepper@mr-pixel.de>
2024-09-07 20:48:34 +0200

    Update ExploreView.vue

--- src/views/db/ExploreView.vue
+++ src/views/db/ExploreView.vue
@@ -2,31 +2,41 @@
   <div class="explore-container">
     <div class="explore-view">
       <div class="left-block background-card">
-        <AnimeSlider
-            filter="date"
-            sort="desc"
-            limit="20"
-        />
-        <AnimeGrid
-            filter="date"
-            sort="desc"
-            initial-count="20"
-        />
-      </div>
+        <div class="newest">
+          <h2>Newest Uploads</h2>
+          <AnimeSlider
+              filter="date"
+              sort="desc"
+              limit="20"
+          />
 
-    </div>
+        </div>
+        <div class="popular">
+          <h2>Currently Popular</h2>
+
+          <AnimeSlider
+              filter="popular"
+              sort="desc"
+              limit="20"
+          />
+        </div>
+
+
+      </div>
+      <div class="right-block background-card">
+</div>
+
+      </div>
   </div>
 
 </template>
 
 <script>
-import AnimeGrid from '@/components/db/AnimesGrid.vue';
 import AnimeSlider from "@/components/db/AnimeSlider.vue";
 
 export default {
   components: {
     AnimeSlider,
-    AnimeGrid,
   },
 };
 </script>
@@ -42,9 +52,23 @@ export default {
   margin-top: 80px;
   max-width: 1000px;
   width: 100%;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
 }
 
 
+
+
+.left-block {
+  flex: 3;
+  width: 100%;
+}
+
+.right-block {
+  flex: 1; /* Adjust this value based on how much space you want */
+  width: 100%; /* Optional, makes sure it uses the full flex basis */
+}
 
 
 
