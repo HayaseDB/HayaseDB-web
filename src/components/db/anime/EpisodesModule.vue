2b6256b9b1a1abf52618db5d71e1a6055a750afd
Sebastian-Stepper <Sebastian.Stepper@mr-pixel.de>
2024-09-08 20:28:18 +0200

    Update EpisodesModule.vue
    episodes table added

--- src/components/db/anime/EpisodesModule.vue
+++ src/components/db/anime/EpisodesModule.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="episodes-module background-card-child">
     <label class="card-title">Episodes</label>
-    <div class="episodes-container">
-      {{ episodes }}
+    <div class="table-container">
+      <table class="episodes-table">
+        <thead>
+        <tr>
+          <th>Episode Number</th>
+          <th>Title</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="episode in episodes" :key="episode.number">
+          <td>{{ episode.number }}</td>
+          <td>{{ episode.title }}</td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -12,18 +25,53 @@ export default {
   name: 'EpisodesModule',
   props: {
     episodes: {
-      type: [Number, String],
-      required: true
+      type: Array,
+      required: true,
+      validator(value) {
+        return value.every(
+            episode => 'number' in episode && 'title' in episode
+        );
+      }
     }
   }
 };
 </script>
 
 <style scoped>
-.episodes-container {
-  display: flex;
-  flex-direction: row;
-  gap: 6px;
-  margin-top: 5px;
+
+.card-title {
+  font-weight: bold;
+}
+
+.table-container {
+  width: 100%;
+  margin-top: 10px;
+}
+
+.episodes-table {
+  width: 100%;
+  border-collapse: collapse;
+  table-layout: auto;
+}
+
+.episodes-table th, .episodes-table td {
+  padding: 0.6em;
+  text-align: left;
+  border-bottom: 1px solid var(--background-300);
+  font-size: 0.875em;
+}
+
+.episodes-table th {
+  background-color: var(--background-200);
+  font-size: 0.9em;
+}
+
+.episodes-table td {
+  white-space: normal;
+  word-wrap: break-word;
+}
+
+.episodes-table tr:nth-child(even) {
+  background-color: var(--background-50);
 }
 </style>
