8ec3edded83469e10807ddb58b06f9106b6c6ab8
Sebastian-Stepper <Sebastian.Stepper@mr-pixel.de>
2024-09-28 21:39:42 +0200

    still in dev mode message

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,28 @@
     <FooterSection />
   </footer>
 
+  <MessageBox
+      v-if="isMessageVisible"
+      message="Be careful! This project is still in early development, visit <a href='https://hayasedb.com' target='_blank' rel='noopener noreferrer'>HayaseDB.com</a> for more details."
+      variant="danger"
+      @close="handleCloseMessageBox"
+  />
+
   <CookieBanner />
 </template>
 
 <script>
-import { computed } from 'vue';
+import { computed, ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import NavBar from './components/NavBar.vue';
 import FooterSection from './components/FooterSection.vue';
 import CookieBanner from './components/CookieBanner.vue';
+import MessageBox from "@/components/general/pushMessage.vue";
 
 export default {
   name: 'App',
   components: {
+    MessageBox,
     NavBar,
     FooterSection,
     CookieBanner,
@@ -34,9 +43,26 @@ export default {
     const showNavBar = computed(() => route.meta.showNavBar !== false);
     const showFooter = computed(() => route.meta.showFooter !== false);
 
+    const isMessageVisible = ref(false);
+
+    onMounted(() => {
+      const messageShown = localStorage.getItem('messageShown');
+
+      if (!messageShown) {
+        isMessageVisible.value = true;
+      }
+    });
+
+    const handleCloseMessageBox = () => {
+      isMessageVisible.value = false;
+      localStorage.setItem('messageShown', 'true');
+    };
+
     return {
       showNavBar,
-      showFooter
+      showFooter,
+      isMessageVisible,
+      handleCloseMessageBox,
     };
   }
 }
