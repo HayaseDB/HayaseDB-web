c900e5b8b95f49f2905a4e264ca8e27382ecc860
Sebastian-Stepper <Sebastian.Stepper@mr-pixel.de>
2025-04-21 20:47:36 +0200

    Update index.ts

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -14,9 +14,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior() {
+  scrollBehavior(to, from, savedPosition) {
     return { top: 0 };
-  }
+  },
 });
 
 export default router;
