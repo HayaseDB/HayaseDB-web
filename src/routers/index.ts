3f0bf4a7e8001d151832d2df0fb6e6cd515ad410
Sebastian-Stepper <Sebastian.Stepper@mr-pixel.de>
2025-04-21 20:32:10 +0200

    better banner and better scroll behav

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -14,12 +14,8 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    } else {
-      return { top: 0 };
-    }
+  scrollBehavior() {
+    return { top: 0 };
   }
 });
 
