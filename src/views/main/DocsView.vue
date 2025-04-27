0a7d68eed9bbb8ed8a54aa419433a9b507a9caf1
AIO <69240351+aiomayo@users.noreply.github.com>
2025-04-27 16:46:16 +0200

    post api url

--- src/views/main/DocsView.vue
+++ src/views/main/DocsView.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="flex flex-col flex-1">
     <iframe
-        ref="iframe"
-        :src="iframeSrc"
-        class="flex-1 w-full border-none"
+      ref="iframe"
+      :src="iframeSrc"
+      class="flex-1 w-full border-none"
+      @load="sendDataToIframe"
     ></iframe>
   </div>
 </template>
 
 <script setup>
-const iframeSrc = '/stoplight/stoplight.html';
+import { ref } from "vue";
+
+const iframeSrc = "/stoplight/stoplight.html";
+const iframe = ref(null);
+
+const sendDataToIframe = () => {
+  iframe.value.contentWindow.postMessage(
+    import.meta.env.VITE_API_BASE_URL + "/doc.json",
+    "*",
+  );
+};
 </script>
