e08e92e1db6df17b28da50dc33b13220cc8a8445
AIO <69240351+aiomayo@users.noreply.github.com>
2025-04-27 16:21:46 +0200

    add stoplight docs

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -94,10 +94,8 @@
       </div>
     </div>
 
-    <main class="flex-1 overflow-y-auto">
-      <div>
-        <router-view />
-      </div>
+    <main class="flex-1 flex flex-col overflow-y-auto">
+        <router-view/>
     </main>
 
     <footer class="bg-white border-t border-gray-200 py-6 px-4 sm:px-6 lg:px-8">
--- src/views/main/DocsView.vue
+++ src/views/main/DocsView.vue
@@ -1,4 +1,13 @@
 <template>
-  <div>Docs</div>
+  <div class="flex flex-col flex-1">
+    <iframe
+        ref="iframe"
+        :src="iframeSrc"
+        class="flex-1 w-full border-none"
+    ></iframe>
+  </div>
 </template>
-<script setup lang="ts"></script>
+
+<script setup>
+const iframeSrc = '/stoplight/stoplight.html';
+</script>
